3f2d688c938195639de1a5514b90713fc4bbfdd6
VivekDeyVD <vivekvd35@gmail.com>
2024-03-20 21:48:17 +0530

    java script

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+name ="tony Stark";
+console.log(name);
+let i=10;
+console.log(i);
+typeof(i);
+console.log(i);
---
+++ javascript.js
@@ -0,0 +1,8 @@
+// const product ={
+//     title : "Ball pen",
+//     rating : 4 ,
+//     offer : 5 ,
+//     price : 1000 ,
+// };
+
+// console.log(product);
